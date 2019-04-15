c6cfa828565e885d019a5facff4f4414280f6bc8
髙橋　綾太 <ry-takahashi@i-enter.co.jp>
2019-04-15 12:08:31 +0900

    GoForwardUnityChanプロジェクト 初めてのコミット
    CubeControllerスクリプトにCubeがぶつかった時の音を再生する処理を書きました。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	// キューブの移動速度
+	private float speed = -0.2f;
+
+	// 消滅位置
+	private float deadLine = -10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// キューブを移動させる
+		transform.Translate(this.speed, 0,0);
+
+		// 画面外に出たら破棄
+		if (transform.position.x < this.deadLine) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		
+		if (collision.gameObject.tag == "CubeTag" ||
+			collision.gameObject.tag == "GroundTag") {
+
+			// ぶつかった音を鳴らす
+			GetComponent<AudioSource>().Play();
+
+		}
+
+	}
+}
